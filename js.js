a2f80bfc74419528b5c6b2a5298be25b306bb2db
AndresFernandoVargasTabaresADSO <anonymatusrs@gmail.com>
2022-09-28 15:36:48 -0500

    Ejercicio 8

--- js.js
+++ js.js
@@ -1,38 +1,30 @@
 addEventListener("DOMContentLoaded", () => {
-    console.log(`7. Una compañía de seguros tiene contratados a n vendedores. Cada uno hace tres ventas a la semana.
-    Su política de pagos es que un vendedor recibe un sueldo base, y un 10% extra por comisiones de
-    sus ventas. El gerente de su compañía desea saber cuanto dinero obtendrá en la semana cada
-    vendedor por concepto de comisiones por las tres ventas realizadas, y cuanto tomando en cuenta su
-    sueldo base y sus comisiones.`)
+    console.log(`8. Diseñar un pseudocódigo que calcule el promedio ponderado para alumno del ITT. El cálculo se hace
+    de la siguiente forma:
+    Se multiplica cada calificación por los créditos de cada materia
+    El resultado anterior se suma con los resultados de todas las materias, por separado se suman los
+    créditos de cada materia y finalmente se divide la suma de todas las materias por sus respectivos
+    créditos, entre la suma de todos los créditos. (materias: Fundamentos, BD y ética).`)
 
     
+    let estado = 'si'
     let i = 1
-    
-    let sueldoBase = Number(prompt(`Ingresa el sueldo base: `))
-    let vendedores = Number(prompt(`Cantidad Vendedores: `))
-    
-    while(vendedores>0){
-        let ventas = Number(prompt(`Ingresa el precio de venta #1: `))
-        let ventas2 = Number(prompt(`Ingresa el precio de venta #2: `))
-        let ventas3 = Number(prompt(`Ingresa el precio de venta #3: `))
-        
-        comision = ventas * 0.10
-        comision2 = ventas2 * 0.10
-        comision3 = ventas3 * 0.10
-        
-        let acum = 0
-        acum+=comision + comision2 + comision3
 
-        vendedores -= 1
-        extra = sueldoBase+acum
-        console.log(`-----------------Vendedor ${i++}-------------------------`)
-        console.log(`Comision de la Venta 1: ${comision}`)
-        console.log(`Comision de la Venta 2: ${comision2}`)
-        console.log(`Comision de la Venta 3: ${comision3}`)
-        console.log(`Comision: ${acum}`)
-        console.log(`Sueldo base: ${sueldoBase}`)
-        console.log(`El sueldo mas la comision es: ${extra}`)
+    while (estado != 'no'){ 
+        nota = parseFloat(prompt("Ingrese la nota de fundamentos: "))
+        cred1 = Number(prompt("Ingrese los creditos de la materia Fundamentos: "))
+        nota2 = parseFloat(prompt("Ingrese la nota de Base de datos: "))
+        cred2 = Number(prompt("Ingrese los creditos de Bases de datos: "))
+        notaE = parseFloat(prompt("Ingrese la nota de Etica: "))
+        credE = Number(prompt("Ingrese los creditos de Etica: "))
+
+        let fin = nota * cred1             
+        let fin2 = nota2 * cred2
+        let finE = notaE * credE
+        let finC = cred2 + credE + cred1
+
+        prom = (fin + fin + finE) / finC   
+        console.log(`Promedio ponderado del estudiante #${i++}: ${Math.round(prom)}`)
+        estado = String(prompt(`Desea agregar las notas de un alumno? (si/no): `)) 
     }
-
-
 })
