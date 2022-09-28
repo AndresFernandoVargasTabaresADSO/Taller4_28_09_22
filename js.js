4579ff4e4c3185aaccbd63580b66145e73976c4e
AndresFernandoVargasTabaresADSO <anonymatusrs@gmail.com>
2022-09-28 14:51:57 -0500

    Ejercicio 6

--- js.js
+++ js.js
@@ -1,32 +1,19 @@
 addEventListener("DOMContentLoaded", () => {
-    console.log(`5. El profesor de una materia desea conocer la cantidad de sus alumnos que no tienen derecho al examen
-    de nivelación. Diseñe un pseudocódigo que lea las calificaciones obtenidas en las 5 unidades por cada
-    uno de los 40 alumnos y escriba la cantidad de ellos que no tienen derecho al examen de nivelación.`)
+    console.log(`6. Un avión que viaja 800 Km/hr. Dispara un proyectil auto impulsado, en el momento del disparo, el
+    avión hace un giro de 90 grados y acelera a 20 mtrs/seg2. El proyectil sigue su curso, acelerando a 10
+    mtrs./seg2. Diseñe un pseudocódigo que escriba cada segundo, la distancia que separa al avión del
+    proyectil, hasta que estén a 10,000 mtrs. o más.`)
 
 
-    let valiste=0
-    let finos=0
-    let alumnos=0
-    i=1
+    let distancia = 0 
+    let tiempo = 0
+while (distancia < 10000){
+    tiempo += 1
+    let A = (800 * tiempo) + (20*tiempo**2)/2 
+    let M = (800 * tiempo) + (10*tiempo**2)/2
+    distancia = Math.sqrt(A**2 + M**2)  
 
-    while(alumnos<40){
-        let nota1 = Number(prompt(`Ingresa tu nota #1: `))
-        let nota2 = Number(prompt(`Ingresa tu nota #2: `))
-        let nota3 = Number(prompt(`Ingresa tu nota #3: `))
-        let nota4 = Number(prompt(`Ingresa tu nota #4: `))
-        let nota5 = Number(prompt(`Ingresa tu nota #5: `))
-        i+=1
-        let promedio= (nota1 + nota2 + nota3 + nota4 + nota5) / 5
-
-        if(promedio<=6){
-            valiste += 1
-        }
-
-        else{
-            finos += 1
-        }
-        alumnos+=1
+    console.log(`Segundo: ${tiempo}`)
+    console.log(`Distancia: ${Math.round(distancia,2)}`)
     }
-    console.log(`Alumnos sin derecho al examen de nivelación: ${valiste}`)
-    console.log(`Alumnos con derecho al examen de nivelación: ${finos}`)
 })
