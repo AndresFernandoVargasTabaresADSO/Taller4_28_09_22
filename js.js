594e2fd9a3e819ff62788109df0a3fdf774dc392
AndresFernandoVargasTabaresADSO <anonymatusrs@gmail.com>
2022-09-28 15:28:39 -0500

    Ejercicio 7

--- js.js
+++ js.js
@@ -1,19 +1,38 @@
 addEventListener("DOMContentLoaded", () => {
-    console.log(`6. Un avión que viaja 800 Km/hr. Dispara un proyectil auto impulsado, en el momento del disparo, el
-    avión hace un giro de 90 grados y acelera a 20 mtrs/seg2. El proyectil sigue su curso, acelerando a 10
-    mtrs./seg2. Diseñe un pseudocódigo que escriba cada segundo, la distancia que separa al avión del
-    proyectil, hasta que estén a 10,000 mtrs. o más.`)
+    console.log(`7. Una compañía de seguros tiene contratados a n vendedores. Cada uno hace tres ventas a la semana.
+    Su política de pagos es que un vendedor recibe un sueldo base, y un 10% extra por comisiones de
+    sus ventas. El gerente de su compañía desea saber cuanto dinero obtendrá en la semana cada
+    vendedor por concepto de comisiones por las tres ventas realizadas, y cuanto tomando en cuenta su
+    sueldo base y sus comisiones.`)
 
+    
+    let i = 1
+    
+    let sueldoBase = Number(prompt(`Ingresa el sueldo base: `))
+    let vendedores = Number(prompt(`Cantidad Vendedores: `))
+    
+    while(vendedores>0){
+        let ventas = Number(prompt(`Ingresa el precio de venta #1: `))
+        let ventas2 = Number(prompt(`Ingresa el precio de venta #2: `))
+        let ventas3 = Number(prompt(`Ingresa el precio de venta #3: `))
+        
+        comision = ventas * 0.10
+        comision2 = ventas2 * 0.10
+        comision3 = ventas3 * 0.10
+        
+        let acum = 0
+        acum+=comision + comision2 + comision3
 
-    let distancia = 0 
-    let tiempo = 0
-while (distancia < 10000){
-    tiempo += 1
-    let A = (800 * tiempo) + (20*tiempo**2)/2 
-    let M = (800 * tiempo) + (10*tiempo**2)/2
-    distancia = Math.sqrt(A**2 + M**2)  
-
-    console.log(`Segundo: ${tiempo}`)
-    console.log(`Distancia: ${Math.round(distancia,2)}`)
+        vendedores -= 1
+        extra = sueldoBase+acum
+        console.log(`-----------------Vendedor ${i++}-------------------------`)
+        console.log(`Comision de la Venta 1: ${comision}`)
+        console.log(`Comision de la Venta 2: ${comision2}`)
+        console.log(`Comision de la Venta 3: ${comision3}`)
+        console.log(`Comision: ${acum}`)
+        console.log(`Sueldo base: ${sueldoBase}`)
+        console.log(`El sueldo mas la comision es: ${extra}`)
     }
+
+
 })
