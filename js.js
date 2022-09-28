099b3e202f09a95cf7385ac7e9044cecb1180c2e
AndresFernandoVargasTabaresADSO <anonymatusrs@gmail.com>
2022-09-28 14:47:09 -0500

    Ejercicio 5

--- js.js
+++ js.js
@@ -1,20 +1,32 @@
 addEventListener("DOMContentLoaded", () => {
-    console.log(`4. 42 personas toman parte de un baile. Durante la fiesta una dama bailó con 7 caballeros, una segunda
-    dama con 8, una tercera con 9, y así sucesivamente hasta que la última bailó con todos los hombres.
-    Cuantas damas había en el baile?`)
+    console.log(`5. El profesor de una materia desea conocer la cantidad de sus alumnos que no tienen derecho al examen
+    de nivelación. Diseñe un pseudocódigo que lea las calificaciones obtenidas en las 5 unidades por cada
+    uno de los 40 alumnos y escriba la cantidad de ellos que no tienen derecho al examen de nivelación.`)
 
 
-    let mujeres = 0 
-    let personas = 0
-    let hombres = 0
+    let valiste=0
+    let finos=0
+    let alumnos=0
+    i=1
 
-    while (personas <= 42){ 
-        mujeres += 1
-        hombres = mujeres + 7  
-        personas = mujeres + hombres  
+    while(alumnos<40){
+        let nota1 = Number(prompt(`Ingresa tu nota #1: `))
+        let nota2 = Number(prompt(`Ingresa tu nota #2: `))
+        let nota3 = Number(prompt(`Ingresa tu nota #3: `))
+        let nota4 = Number(prompt(`Ingresa tu nota #4: `))
+        let nota5 = Number(prompt(`Ingresa tu nota #5: `))
+        i+=1
+        let promedio= (nota1 + nota2 + nota3 + nota4 + nota5) / 5
+
+        if(promedio<=6){
+            valiste += 1
+        }
+
+        else{
+            finos += 1
+        }
+        alumnos+=1
     }
-
-console.log(`Personas: ${personas} `)
-console.log(`Mujeres: ${mujeres}`)
-console.log(`Hombres: ${hombres}`)
+    console.log(`Alumnos sin derecho al examen de nivelación: ${valiste}`)
+    console.log(`Alumnos con derecho al examen de nivelación: ${finos}`)
 })
